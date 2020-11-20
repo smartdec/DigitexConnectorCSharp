8f7aed99eba240da38fa34d0fa7f600c67e92a9e
Artemov Vitaliy <48940708+vitaliy-art@users.noreply.github.com>
2020-11-20 20:45:30 +0300

    Update Program.cs

--- Example/Program.cs
+++ Example/Program.cs
@@ -9,7 +9,7 @@ namespace Example
             // for mainnet set host to "ws.mainnet.digitexfutures.com"
             string host = "ws.testnet.digitexfutures.com";
             bool wss = true;
-            string token = "d030b17c9b993107399eb19121daea0f04e4b476";
+            string token = "<your_API_token>";
 
             IntervalAlgorithm algorithm = new IntervalAlgorithm(host, token, wss);
             algorithm.Prepare();
